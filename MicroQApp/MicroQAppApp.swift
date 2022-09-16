83010b5a2119d4529bf43253c3496f53433df242
Ryan Du <ryandu9221@gmail.com>
2022-09-16 12:13:30 -0400

    Initial Commit

---
+++ MicroQApp/MicroQAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroQAppApp.swift
+//  MicroQApp
+//
+//  Created by Ryan D on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MicroQAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
